039db573f3ba465e30b10ff2bf79ecb350b14fea
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-05-08 19:45:41 +0100

    code formatted

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,14 @@
-const input =document.querySelector('.input-search')
-const form = document.querySelector ('form')
+const input = document.querySelector(".input-search");
+const form = document.querySelector("form");
 const data = new CountryData();
-const display= document.querySelector('.display')
+const display = document.querySelector(".display");
 
+const updateUi = (data) => {
+  const { countryDetail } = data;
 
-const updateUi=(data)=>{
-  
-const {countryDetail}=data
+  const imgSrc = `${countryDetail.flag}`;
 
-const imgSrc =`${countryDetail.flag}`
-
-display.innerHTML=`<div class="row">
+  display.innerHTML = `<div class="row">
 <div class='b-card'>
   <div class="row">
     <div class='lg-col-6 image'>
@@ -21,14 +19,28 @@ display.innerHTML=`<div class="row">
       <div class="row">
         <div class='col-12 lg-col-6 content-one'>
           <h3> ${countryDetail.name}</h3>
-          <P><span class='bold-text'>Native Name: </span>${countryDetail.nativeName}</P>
-          <P><span class='bold-text'>Population: </span>${countryDetail.population}</P>
+          <P><span class='bold-text'>Native Name: </span>${
+            countryDetail.nativeName
+          }</P>
+          <P><span class='bold-text'>Population: </span>${
+            countryDetail.population
+          }</P>
           <P><span class='bold-text'>Region: </span>${countryDetail.region}</P>
-          <P><span class='bold-text'>Sub Region: </span>${countryDetail.subregion}</P>
-          <P><span class='bold-text'>Capital: </span>${countryDetail.capital}</P>
-          <P><span class='bold-text'>Top Level Domain: </span>${countryDetail.topLevelDomain}</P>
-          <P><span class='bold-text'>Currencies: </span>${countryDetail.currencies.map(currency=>currency.code)}</P>
-          <P><span class='bold-text'>Languages:${countryDetail.languages.map(language=>language.name)} </span></P>
+          <P><span class='bold-text'>Sub Region: </span>${
+            countryDetail.subregion
+          }</P>
+          <P><span class='bold-text'>Capital: </span>${
+            countryDetail.capital
+          }</P>
+          <P><span class='bold-text'>Top Level Domain: </span>${
+            countryDetail.topLevelDomain
+          }</P>
+          <P><span class='bold-text'>Currencies: </span>${countryDetail.currencies.map(
+            (currency) => currency.code
+          )}</P>
+          <P><span class='bold-text'>Languages:${countryDetail.languages.map(
+            (language) => language.name
+          )} </span></P>
         </div>
         <div class='col-12 lg-col-12 content-three'>
           <p><span class='bold-text'>Border Countries: </span><span class='border'>Border nav3 </span></p>
@@ -43,24 +55,19 @@ display.innerHTML=`<div class="row">
 
 </div>
 
-`
-}
+`;
+};
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const country = form.input.value.trim();
+  form.reset();
 
+  data
+    .updateCountry(country)
+    .then((data) => {
+      updateUi(data);
+    })
 
-form.addEventListener('submit',(e)=>{
-  e.preventDefault()
-  const country = form.input.value.trim()
-  form.reset()
-
-data.updateCountry(country)
-.then(data=>{
- 
-  updateUi(data)})
-
-  .catch(err =>console.log(err))
-  
-  
-})
-
-
+    .catch((err) => console.log(err));
+});
