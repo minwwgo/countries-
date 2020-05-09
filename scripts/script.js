81fbe63c2872a421915ef522d363ce26c221ea2e
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-05-09 07:55:39 +0100

    code format

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,9 @@ const input = document.querySelector(".input-search");
 const form = document.querySelector("form");
 const data = new CountryData();
 const display = document.querySelector(".display");
-const row = document.createElement('div')
-row.classList.add('row')
+const row = document.createElement("div");
+row.classList.add("row");
+const selectBox = document.querySelector("select-box");
 
 const updateUi = (data) => {
   const { countryDetail } = data;
@@ -66,35 +67,38 @@ form.addEventListener("submit", (e) => {
   data
     .updateCountry(country)
     .then((data) => {
-        updateUi(data);
-        
+      updateUi(data);
     })
 
     .catch((err) => console.log(err));
 });
 
-data.getAllCData()
-.then(data=>{data.forEach(country=>{
-  updatehomepage(country)
-  })
-  .catch((err) => console.log(err));
-})
-const updatehomepage=(country)=>{
-  const home= document.createElement('div')
-  let classToAdd = ['col-12','sm-col-12','md-col-12','lg-col-3','card']
-  home.classList.add(...classToAdd)
-  const imgSrc = `${country.flag}`
-  home.innerHTML= `
+data.getAllCData().then((data) => {
+  data
+    .forEach((country) => {
+      updatehomepage(country);
+    })
+    .catch((err) => console.log(err));
+});
+const updatehomepage = (country) => {
+  const home = document.createElement("div");
+  let classToAdd = ["col-12", "sm-col-12", "md-col-12", "lg-col-3", "card"];
+  home.classList.add(...classToAdd);
+  const imgSrc = `${country.flag}`;
+  home.innerHTML = `
     <img src=${imgSrc}>
-    
     <p><span class="bold-text">Population: </span>${country.population} </p>
     <p><span class="bold-text">Region: </span>${country.region}</p>
     <p><span class="bold-text">Capital: </span>${country.capital}</p>
-    `
-  display.append(row)
-  row.append(home)
-}
+    `;
+  display.append(row);
+  row.append(home);
+};
 
-
-
-
+data.getRegion().then((data) => {
+  data.forEach((region) => {
+    const option = document.createElement("option");
+    option.innerHTML = `${region}`;
+    selectBox.append(option);
+  });
+});
